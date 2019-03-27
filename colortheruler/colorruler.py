35013cb3c628049c4a2547a120cdf1fe3d278aa6
HARIHARAN <adshari22@gmail.com>
2019-03-27 15:57:09 +0530

    Validation rules applied on each input

--- colortheruler/colorruler.py
+++ colortheruler/colorruler.py
@@ -3,25 +3,35 @@ import random
 #give ruler size randomly(15,30,45)
 size = random.sample(set([15 ,30 , 45]), 1)
 print("Ruler size is ",size[0])
+
+def getinput():
+        while True:
+            try:
+                temp = int(input())
+                if (temp > 0):
+                  break
+                else:
+                  print ("Sorry, Atleast each color should be prestnt once. Enter number of boxes again")
+                  continue
+            except ValueError:
+                print("Sorry, only integers are allowed. Enter number of boxes again")
+                continue
+        return temp
+
 while True:
-    try:
-        #give input number of r,g,b color boxes
-        r=int(input("Enter Number of Red Boxes(1 cm) "))
-        g=int(input("Enter Number of Green Boxes(5 cm) "))
-        b=int(input("Enter Number of Blue Boxes(3 cm) "))
 
-        if (r<1 or g<1 or b<1) :
-            print ("Atleast each color should be prestnt once")
+    #give input number of r,g,b color boxes
+    print("Enter Number of RedBoxes ")
+    r=getinput()
+    print("Enter Number of GreenBoxes ")
+    g=getinput()
+    print("Enter Number of BlueBoxes ")
+    b=getinput()
 
-        else:    
-            #check whether boxes cover the complete ruler
-            if size[0] == r*1+g*5+b*3 :
-                print("correct")
-            else:
-                print("wrong")
-            break
-    except ValueError:
-        print("Sorry, enter a vaild entry")
-        continue
-
-
+    #check whether boxes cover the complete ruler
+    if size[0] == r*1+g*5+b*3 :
+      print("correct")
+    else:
+      print("wrong")
+      
+    break
