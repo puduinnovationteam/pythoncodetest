8a75136ec7d95a2bce18ed6fdae32ee47017aa5f
Yunus0497 <yunus0497@gmail.com>
2019-03-27 16:07:58 +0530

    Reduced redundancy by creating a method.

--- colortheruler/colortheruler.py
+++ colortheruler/colortheruler.py
@@ -1,52 +1,39 @@
 import random  # random module to generate a random number
 
-start = input("Start Game (Y/N)?"); 
 
-if(start == "Y"):
+
+def inputNumber(boxColor):
+  while True:
+    try:
+      userInput = int(input(boxColor))
+    except ValueError:
+      print("Not an integer! Try again.")
+      continue
+    if userInput < 1:
+      print("Atleast 1 box should be painted in this color");continue;
+    else:
+      return userInput
+      break
+
+start = input("Start Game (Y/N)?\n"); 
+
+if(start == "Y" or start == "y"):
   while True:
     ruler = random.randint(1,3)*15;
     print("Fill the ruler of ",ruler," cm with 3 color boxes")
-    while True:
-      try:
-        red = int(input("Red boxes :"))*1
-      except ValueError:
-        print("Enter a valid entry!")
-        continue;
-      if red < 1:
-        print("Atleast 1 red box should be painted")
-        continue;
-      else:
-        break;
-    while True:
-      try:
-        blue = int(input("Blue boxes : "))*3
-      except ValueError:
-        print("Enter a valid entry!")
-        continue;
-      if blue < 1:
-        print("Atleast 1 blue box should be painted")
-      else:
-        break;
-    while True:
-      try:
-        green = int(input("Green boxes : "))*5
-      except ValueError:
-        print("Enter a valid entry!")
-        continue;
-      if green < 1:
-        print("Atleast 1 blue box should be painted")
-      else:
-        break;
+    red = inputNumber("Red Boxes:")*1
+    blue = inputNumber("Blue boxes : ")*3
+    green = inputNumber("Green boxes : ")*5
+    
     if((red+blue+green) == ruler):
       print("Hurray! Correct")
     else:
       print("Oops! Wrong")
 
-    playagain = input("Play again (Y/N)?")
+    playagain = input("Play again (Y/N)?\n")
 
-    if(playagain =="N"):
+    if(playagain =="N" or playagain == "n"):
       print("Thanks for playing!!")
       break
-
 else:
   print("You missed it :(")
